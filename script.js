ce8fc2997a66b9bfd1b9b765c7dc4a6e92da39bc
mattlusty <mattlusty123@gmail.com>
2023-11-08 15:05:16 +0000

    Calc each months Profit/Loss change

--- script.js
+++ script.js
@@ -14,3 +14,16 @@ for (i in finances) {
 }
 
 console.log("Total Profit/Loss:", sum);
+
+// Profit/Loss changes per month
+
+// Previous month before first month - Profit unknown
+finances[0][2] = null;
+
+for (var i = 1; i < finances.length; i++) {
+  finances[i - 1][2] = finances[i - 1][1] - finances[i][1];
+}
+
+console.log("Finances:", finances);
+
+console.log("----------");

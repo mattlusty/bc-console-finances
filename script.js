7ec5b107a4eab0f30ff771f06af6c6591ca87699
mattlusty <mattlusty123@gmail.com>
2023-11-08 15:04:33 +0000

    Total Profit/Loss

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 var months = finances.length;
 
 console.log("Total Months:", months);
+
+console.log("----------");
+
+var sum = 0;
+
+// Total Profit/Loss
+
+for (i in finances) {
+  // console.log(finances[i][1]);
+  sum += finances[i][1];
+}
+
+console.log("Total Profit/Loss:", sum);
